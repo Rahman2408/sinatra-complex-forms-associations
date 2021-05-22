c09bdd4582e64662df748d06d731ba2979dcb27c
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-05-22 01:29:27 -0400

    commit tree 442d10f4f7cef6a8993e21e831e4ab4b3d95bb74

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,16 +1,23 @@
 class PetsController < ApplicationController
 
   get '/pets' do
+    @owners = Owner.all
     @pets = Pet.all
     erb :'/pets/index' 
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+  
+    @pet = Pet.create(name: params["pet"]["name"])
+    if !params["owner"].empty? 
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    binding.pry
     redirect to "pets/#{@pet.id}"
   end
 
